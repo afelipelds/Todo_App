8b1085bb648e1d5d8ca75bed5017316d2ba0f893
andres eslava <aeslava@gearplug.io>
2020-08-05 01:53:07 -0500

    First view of Todo_App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import  Home  from './components/Home'
+import { AddTodo, DeleteTodo, EditTodo } from './components/Todo'
+import './assets/scss/index.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <Router>
+      <h2>Nav</h2>
+      <Switch>
+        <Route exact path={"/"} component={Home} />
+        <Route path={"/add"} component={AddTodo} />
+        <Route path={"/delete"} component={DeleteTodo} />
+        <Route path={"/edit/:id"} component={EditTodo} />
+        
+      </Switch>
+    </Router>
+  </>);
 }
 
 export default App;
